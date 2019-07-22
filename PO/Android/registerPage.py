1cd71f368d47cc48d37e7e34b36a710808f390d6
xieguoqianglove <761633479@qq.com>
2019-07-22 10:33:02 +0800

    更新注册的元素定位

--- PO/Android/registerPage.py
+++ PO/Android/registerPage.py
@@ -1,5 +1,8 @@
 from PO.basePage import Base
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
@@ -9,76 +12,102 @@ class registerPage(Base):
     """
 
     register_button = (By.ID, "btnSignup")  # 启动页注册按钮
-    select_nation_btn = (By.ID, "tv_encrytionType")  # 国家下拉框
-    send_mobile = (By.ID, "et_phone_number")  # 注册手机号码
-    next_step = (By.ID, "btn_signUp")  # 注册第一步
-    mobile_code = (By.ID, "et_mobile_code")  # 手机验证码
-    verify_code = (By.ID, "bt_moblie_code_verify_next")  # 注册第二步
-    register_pwd = (By.ID, "et_register_pass")  # 登录密码
-    comfirm_pwd = (By.ID, "et_register_pass_again")  # 确认登录密码
-    comfirm_btn = (By.ID, "btn_completeRegistration")  # 注册第三步
-    pay_pwd = (By.ID, "et_register_pass")  # 支付密码
-    comfirm_pay_pwd = (By.ID, "et_register_pass_again")  # 确认支付密码
-    complete = (By.ID, "btn_completeRegistration")  # 完成
-    tt = (By.ID, "tv_mobile_verify_time")  # 倒计时
-    switch_email = (By.ID, "tv_switch")
+    switch_mobile_register = (By.ID,'tv_menu') #切换手机注册
+    select_nation_btn = (By.ID, "tv_select_country")  # 国家下拉框
+    input_eamil = (By.ID, "et_email")  # 输入邮箱
+    input_mobile = (By.ID, "et_phone_number")  # 输入手机号码
+    input_login_pwd = (By.ID, "et_login_pass")  # 输入登录密码
+    input_pay_pwd = (By.ID, "et_pay_password")  # 输入支付密码
+    comfirm_pay_pwd = (By.ID, "et_confirm_pay_password")  # 确认支付密码
+    click_verify = (By.ID, "tv_send_code")  # 点击发送验证码
+    input_verify = (By.ID, "ed_code")  # 输入验证码
+    click_confirm = (By.ID, "btn_signup")  # 点击注册提交
+
+    click_service = (By.ID, 'tv_terms_sevice')  # 点击注册服务协议
+    click_user = (By.ID, 'tv_terms_user')  # 点击注册用户协议
+    click_Login = (By.ID, 'tv_accountLogin')  # 点击注册界面的Login跳转登陆界面
 
     def click_register(self):
-        """
-        选择注册
-        """
+        """进入注册页面"""
+        WebDriverWait(self.driver, 10, 0.5).until(
+            EC.text_to_be_present_in_element(self.register_button, u"SIGN UP"))
         self.driver.find_element(*self.register_button).click()
 
-    def register_by_mobile(self, nation, mobile):
-        """
-        1.选择国家
-        2.手机号码注册
-        """
-
-        self.select_nation(nation)
-        time.sleep(1)
-        self.driver.find_element(*self.send_mobile).send_keys(mobile)
-        self.driver.find_element(*self.next_step).click()
-        time.sleep(2)
-
-    def select_nation(self, na):
-        """
-        选择国家
-        """
-        nation_XPATH = (By.XPATH, f'//android.widget.TextView[contains(@text, "{na}")]')  # 定位国家
-
+    def select_nation(self, nation):
+        """选择国家 """
+        nation_XPATH = (By.XPATH, f'//android.widget.TextView[contains(@text, "{nation}")]')# 定位国家
         self.driver.find_element(*self.select_nation_btn).click()
-        while not self.findElement(na):
+        time.sleep(3)
+        while not self.findElement(nation):
+            self.swipeUp(duration=1500)
             self.swipeUp(duration=1500)
         else:
             self.driver.find_element(*nation_XPATH).click()
+            # WebDriverWait(self.driver, 10, 0.5).until(
+            #     EC.text_to_be_present_in_element(nation_XPATH, f"{nation}"))
+            # self.driver.find_element(*nation_XPATH).click()
 
-    def verify(self, code=2222):
-        """
-        输入手机验证码
-        """
-
-        self.driver.find_element(*self.mobile_code).send_keys(code)  # 测试环境默认2222
-        self.driver.find_element(*self.verify_code).click()  # 第二步
+    def send_loginpwd_paypwd_code(self,loginpwd,paypwd,code):
+        """输入登陆密码、支付密码、手机验证码"""
+        self.driver.find_element(*self.input_login_pwd).send_keys(loginpwd)
+        self.driver.find_element(*self.input_pay_pwd).send_keys(paypwd)
+        self.driver.find_element(*self.comfirm_pay_pwd).send_keys(paypwd)
+        self.driver.back()
         time.sleep(2)
+        self.driver.find_element(*self.click_verify).click() # 点击发送验证码
+        time.sleep(2)
+        self.driver.find_element(*self.input_verify).send_keys(code)  # 验证码默认2222
+        self.driver.back()
 
-    def send_pwd(self, pwd='Aa123456'):
-        """
-        注册时输入账号密码
-        """
-
-        self.driver.find_element(*self.register_pwd).send_keys(pwd)
-        self.driver.find_element(*self.comfirm_pwd).send_keys(pwd)
-        self.Sys_back()  # 缩下键盘
-        self.driver.find_element(*self.comfirm_btn).click()  # 第三步
+    def set_payment_password(self,paypwd):
+        '''设置支付密码'''
+        a = self.driver.find_element(*self.input_pay_pwd)
+        # ActionChains创建鼠标事件,move_to_element鼠标移动到某个元素,click单击鼠标左键,send_keys发送某个键到当前焦点的元素,perform执行鼠标事件
+        action_a = ActionChains(self.driver)
+        action_a.move_to_element(a).click().send_keys(paypwd).perform()
         time.sleep(1)
+        b = self.driver.find_element(*self.comfirm_pay_pwd)
+        action_b = ActionChains(self.driver)
+        action_b.move_to_element(b).click().send_keys(paypwd).perform()
 
-    def send_pay_pwd(self, p='123456'):
-        """
-        注册时输入支付密码
-        """
+    def register_by_mobile(self,nation,mobile,loginpwd,paypwd,code):
+        """手机号码注册"""
+        time.sleep(2)
+        self.driver.find_element(*self.switch_mobile_register).click()  # 切换手机号码方式
+        time.sleep(1)
+        self.select_nation(nation)
+        time.sleep(1)
+        self.driver.find_element(*self.input_mobile).send_keys(mobile)  # 输入手机号码
+        self.send_loginpwd_paypwd_code(loginpwd,paypwd,code)  # 输入登陆密码、支付密码、验证码
+        # self.driver.find_element(*self.click_confirm).click()  # 点击注册
+        # time.sleep(5)
 
-        self.driver.find_element(*self.pay_pwd).send_keys(p)
-        self.driver.find_element(*self.comfirm_pay_pwd).send_keys(p)
-        self.driver.find_element(*self.complete).click()  # 完成
+    def register_by_eamil(self,nation,email,loginpwd,paypwd,code):
+        """邮箱注册"""
+        self.select_nation(nation)
+        time.sleep(1)
+        self.driver.find_element(*self.input_eamil).send_keys(email)  # 输入邮箱
+        time.sleep(1)
+        self.send_loginpwd_paypwd_code(loginpwd,paypwd,code)  # 输入登陆密码、支付密码、验证码
+        # self.driver.find_element(*self.click_confirm).click() #点击注册
+        # time.sleep(5)
+
+    def click_confirm_button(self):
+        '''点击注册'''
+        self.driver.find_element(*self.click_confirm).click() #点击注册
+        time.sleep(5)
+
+    def clcik_register_Agreement(self):
+        '''点击注册界面的注册协议'''
+
+        self.driver.back()
+        time.sleep(2)
+        self.driver.find_element(*self.click_service).click()
+        # self.driver.find_element_by_android_uiautomator('.textContains("SERVICE")').click()
+        time.sleep(2)
+        self.driver.back()
+        self.driver.find_element(*self.click_user).click()
+        time.sleep(2)
+        self.driver.back()
+        self.driver.find_element(*self.click_Login).click()
         time.sleep(2)
