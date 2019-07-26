879491b1b2e9431d1edbf8af068b903f71e91ff4
jenny <756574746@qq.com>
2019-07-26 18:29:21 +0800

    提交dappfxcase文件

--- PO/Android/dappFxPage.py
+++ PO/Android/dappFxPage.py
@@ -1,6 +1,8 @@
 from PO.basePage import Base
 from selenium.webdriver.common.by import By
 import time
+from appium import webdriver
+import unittest
 
 
 class DappFxPage(Base):
@@ -14,6 +16,8 @@ class DappFxPage(Base):
     Dapp_enter_fx_card = "FX"  # 点击fxcard
     Dapp_add_card_btn = (By.ID, "ib_add_card")  # 添加卡片按钮
     Dapp_show_amout = (By.ID, "img_show_amout")  # 加密按钮
+    fx_card_btn= (By.XPATH,"//android.widget.LinearLayout[@resource-id='com.pundix.xwallet:id/ll_layout_balance']") #点击进入fx卡
+
 
     # ----------fx卡片主界面元素---------------
     fx_amout = (By.ID, "tv_amout")  # fx卡片里面的加密按钮
@@ -21,9 +25,9 @@ class DappFxPage(Base):
     fx_back_btn = (By.CLASS_NAME, "android.widget.ImageButton")  # fx卡片里面的左上角返回键
     fx_conversion_btn = "Conversion"  # 转换按钮
     fx_staking_btn = "Staking"  # 挖矿按钮
-    FX_btn = "FX"  # fx按
-    fx_NPXS_btn = "NPXS"  # npxs按钮
-    fx_NPXSXEM_btn = "NPXSXEM"  # npxsxem按钮
+    fx_main_btn =(By.XPATH,"//android.widget.TextView[@text='FX']")# fx按钮
+    fx_NPXS_btn = (By.XPATH,"//android.widget.TextView[@text='NPXS']")  # npxs按钮
+    fx_NPXSXEM_btn = (By.XPATH,"//android.widget.TextView[@text='NPXSXEM']")  # npxsxem按钮
 
     # ---------转换功能界面元素-------------
     conver_option_btn = (By.ID, "option")  # 帮助按钮
@@ -42,22 +46,32 @@ class DappFxPage(Base):
     staking_cancle = (By.ID, "cancle")  # 取消按钮
 
     # -----------fx转账界面元素----------
-    fx_transfer = (By.ID, "btn_withdraw")  # 转账按钮
+    fx_page_transfer = (By.ID, "btn_withdraw")  # 转账按钮
     fx_type_name = (By.ID, "tv_type_name")  # 账单名称
+    fx_transferAddress=(By.ID,"ed_transferAddress")#fx转账地址
+    fx_coinNumber=(By.ID,"ed_transferCoinNumber")#输入金额框
     # 扫码按钮
     fx_sacn = (By.XPATH,
                "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.RelativeLayout[2]/android.widget.RelativeLayout/android.widget.ImageView")
     fx_all = (By.ID, "tv_allCoinNumber")  # all按钮
-    fx_transferNext = (By.ID, "btn_transferNext")  # 下一步按钮
+    fx_transferNext = (By.ID, "btn_transferNext")  # 第一个fx界面的下一步按钮
+    fx_withdrawNext=(By.ID,"btn_withdrawNext1")   #第二个fx'转账的下一步按钮
+    fx_email_code=(By.ID,"ed_email_code")#邮箱验证码
+    fx_pay_password=(By.ID,"ed_pay_password") #输入支付密码
+    fx_send_emai_code=(By.ID,"tv_send_email_code")  #点击发送验证码
+    fx_confirm=(By.ID,"btn_confirm") #点击确认按钮
+    fx_bill=(By.XPATH,"//android.widget.TextView[contains(@text,'Distribution')")#fx账单名称
 
     # ------------NPXS界面元素------------
-    npxs_menu = (By.ID, "iv_menu")  # 帮助按钮
+    npxs_help_menu = (By.ID, "iv_menu")  # 帮助按钮
     npxs_transfer = (By.ID, "btn_transfer")  # NPXS转账
     npxs_add = (By.ID, "rl_layout_add")  # add按钮
     npxs_private = (By.ID, "ll_layout_private")  # private wallet account
     npxs_exchange = (By.ID, "iv_exchange")  # 切换按钮
     npxs_all = (By.ID, "tv_available_all")  # 全部按钮
     npxs_confirm = (By.ID, "btn_transfer")  # 确认转账按钮
+    npxs_input_amount=(By.ID,"ed_available")#内部转账金额输入框
+
 
     # ---------NPXSXEM界面元素------------
     npxsxem_menu = (By.ID, "iv_menu")  # 帮助按钮
@@ -115,12 +129,122 @@ class DappFxPage(Base):
         time.sleep(2)
 
     def click_fx_text(self):
+        """
+        点击转换按钮
+        :return:
+        """
         self.driver.find_element(*self.fx_conversion_btn)
 
 
     def enter_conversion(self):
-
         """
         进入转换的帮助按钮
         """
         self.driver.find_element(*self.fx_conversion_btn).click()
+
+    def Dapp_enter_fx(self):
+        """从Dapp界面进入fx转账界面"""
+
+        self.driver.find_element(*self.fx_card_btn).click()  # 点击DAPP界面的fx按钮
+        time.sleep(2)
+    def enter_fx(self):
+        """点击fx卡片界面的fx按钮"""
+        self.driver.find_element(*self.fx_main_btn).click() #点击fx卡片的fx按钮
+        time.sleep(2)
+
+    def fx_transfer(self,text,text2,text3,text4):
+        """
+        fx转账
+        """
+        self.driver.find_element(*self.fx_page_transfer).click()#点击转账按钮
+        time.sleep(2)
+
+        fx_address = self.driver.find_element(*self.fx_transferAddress)#点击fx转账地址
+        fx_address.click();time.sleep(2);fx_address.send_keys(text) #输入转账地址
+        time.sleep(2)
+
+        fx_amount=self.driver.find_element(*self.fx_coinNumber)#点击转账金额输入框
+        fx_amount.click();fx_amount.send_keys(text2)#输入金额
+        #coinNumber = self.driver.find_element(*self.fx_all).click()#点击all按钮
+        # coinNumber.clear()
+        # coinNumber.send_keys(text1)
+        time.sleep(2)
+        self.driver.back()
+        time.sleep(1)
+        self.driver.find_element(*self.fx_transferNext).click()#点击第一个界面的下一步按钮
+        time.sleep(2)
+        self.driver.find_element(*self.fx_withdrawNext).click()#点击第二个界面的下一步按钮
+        time.sleep(2)
+        self.driver.find_element(*self.fx_send_emai_code).click()#点击发送验证码
+        time.sleep(2)
+        email_code =self.driver.find_element(*self.fx_email_code)#点击邮箱验证码输入框
+        email_code.click();email_code.send_keys(text3)
+        time.sleep(2)
+
+        pay_password=self.driver.find_element(*self.fx_pay_password) #点击支付密码
+        pay_password.click();pay_password.send_keys(text4)#输入支付密码
+        time.sleep(3)
+        self.driver.find_element(*self.fx_confirm).click()#点击确认转账按钮
+        time.sleep(2)
+
+    def enter_fx_bill(self):
+        """进入fx账单界面"""
+        self.driver.page_source()
+        msg = self.driver.find_element(*self.fx_bill).text
+        #webdriver.Remote().find_element_by_id().is
+
+
+        print(msg.get_attribute)
+        print(msg.page_source)
+        time.sleep(1)
+        return msg
+
+    def enter_NPXSPage(self):
+        """进入npxs界面"""
+        self.driver.find_element(*self.fx_NPXS_btn).click()
+        time.sleep(2)
+
+    def enter_NPXSXEMPage(self):
+        """进入npxsxem界面"""
+        self.driver.find_element(*self.fx_NPXSXEM_btn).click()
+        time.sleep(2)
+
+    def enter_NPXS_helpBtn(self):
+        """进入npxs界面的帮助按钮"""
+        self.driver.find_element(*self.npxs_help_menu).click()
+        time.sleep(2)
+
+    def enter_NPXS_transfer(self):
+        """点击npxs界面的transfer按钮"""
+        self.driver.find_element(*self.npxs_transfer).click()
+        time.sleep(2)
+
+    def enter_NPXS_Add(self):
+        """点击npxs界面的add按钮"""
+        self.driver.find_element(*self.npxs_add).click()
+
+    def click_transfer_exchange(self):
+        """点击npxs内部划转的切换按钮"""
+        self.driver.find_element(*self.npxs_exchange).click()
+        time.sleep(1)
+
+    def click_all_Internal_transfer(self):
+        """点击内部转账的all按钮"""
+        self.driver.find_element(*self.npxs_all).click()
+        time.sleep(1)
+
+    def input_Internal_transfer_amount(self,text):
+        """输入内部划转金额"""
+        self.driver.find_element(*self.npxs_input_amount).click()
+        time.sleep(2)
+
+        npxs_amount=self.driver.find_element(*self.npxs_input_amount)#在内部转账界面输入金额
+        npxs_amount.click();npxs_amount.send_keys(text)#输入金额
+        time.sleep(2)
+
+        self.driver.find_element(*self.npxs_confirm).click()#点击确认按钮
+        time.sleep(1)
+
+
+
+
