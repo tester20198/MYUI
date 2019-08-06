277bca3d721e9e09f9d5565a725b7b2f3713a9d8
tester20198 <476367001@qq.com>
2019-08-06 18:47:59 +0800

    完成xpass卡测试脚本

--- testcase/Android/XPASScard_case.py
+++ testcase/Android/XPASScard_case.py
@@ -62,30 +62,31 @@ class XPASSTestCase(unittest.TestCase):
     #     self.assertTrue(self.xpass_page.check_QR_code(), '判断付款二维码是否加啊在成功')
     #     time.sleep(1)
     #     self.xpass_page.click_refresh()  # 点击付款二维码刷新按钮
-    #     time.sleep(1)
+    #     time.sleep(2)
     #     try:
     #         self.xpass_page.check_card_Receive()  # 点击充值码并检查
     #         self.xpass_page.save_img(f'/{coin}充值码')
     #     except BaseException:
-    #         print(f'无法获取{coin}充值二维码，请检查截图...')
-
-    def test_004_check_XPASS_BTC_transfer(self, coin='BTC', address='213123123113', money='0.00000001'):
-        """
-        检查XPASS卡的BTC的转账
-        coin：币种，根据币种改变测试用例
-        """
-
-        self.xpass_page.into_XPASS_card()
-        time.sleep(2)
-        self.xpass_page.into_coin_detail(coin)
-        time.sleep(2)
-        self.xpass_page.ERC20_transfer(address, money) # 点击卡片中的转账
-        time.sleep(2)
-        self.xpass_page.send_codes()  # 输入转账时的验证码
-        time.sleep(1)
-        self.xpass_page.confirm_transfer()
-        self.assertTrue(self.xpass_page.is_toast_exist('transfer'), '判断是否转账成功')
+    #         print(f'无法获取{coin}充值二维码...')
     #
+    # def test_004_check_XPASS_BTC_transfer(self, coin='BTC', address='',
+    #                                       money='0.00001'):
+    #     """
+    #     检查XPASS卡的BTC的转账
+    #     coin：币种，根据币种改变测试用例
+    #     """
+    #
+    #     self.xpass_page.into_XPASS_card()
+    #     time.sleep(2)
+    #     self.xpass_page.into_coin_detail(coin)
+    #     time.sleep(2)
+    #     self.xpass_page.ERC20_transfer(address, money)  # 点击卡片中的转账
+    #     time.sleep(2)
+    #     self.xpass_page.send_codes(pwd='123123')  # 输入转账时的验证码
+    #     time.sleep(1)
+    #     self.xpass_page.confirm_transfer()
+    #     self.assertTrue(self.xpass_page.is_toast_exist('transfer'), '判断是否转账成功')
+
     # def test_005_XPASS_ETH_rec(self):
     #     """
     #     检查XPASS卡ETH的充值地址
@@ -93,15 +94,15 @@ class XPASSTestCase(unittest.TestCase):
     #     """
     #
     #     self.test_003_check_XPASS_BTC_rec(coin='ETH')
-    #
+
     # def test_006_check_XPASS_ETH_transfer(self):
     #     """
     #     检查XPASS卡的ETH的转账
     #     coin：币种，根据币种改变测试用例
     #     """
     #
-    #     self.test_004_check_XPASS_BTC_transfer(coin='ETH')
-    #
+    #     self.test_004_check_XPASS_BTC_transfer(coin='ETH', address='0xaA3A007c27F41B7527d339270b3c46EDAF51fe84', money='0.00001')
+
     # def test_007_XPASS_NPXS_rec(self):
     #     """
     #     检查XPASS卡NPXS的充值地址
@@ -109,14 +110,14 @@ class XPASSTestCase(unittest.TestCase):
     #     """
     #
     #     self.test_003_check_XPASS_BTC_rec(coin='NPXS')
-    #
+
     # def test_008_check_XPASS_NPXS_transfer(self):
     #     """
     #     检查XPASS卡的NPXS的转账
     #     coin：币种，根据币种改变测试用例
     #     """
     #
-    #     self.test_004_check_XPASS_BTC_transfer(coin='NPXS')
+    #     self.test_004_check_XPASS_BTC_transfer(coin='NPXS', address='0xaA3A007c27F41B7527d339270b3c46EDAF51fe84', money='0.00001')
     #
     # def test_009_check_XPASS_BNB_rec(self):
     #     """
@@ -125,29 +126,34 @@ class XPASSTestCase(unittest.TestCase):
     #     """
     #
     #     self.xpass_page.into_XPASS_card()
+    #     time.sleep(2)
     #     self.xpass_page.into_coin_detail(coin='BNB')
+    #     time.sleep(2)
     #     try:
-    #         self.xpass_page.save_img('/BNB充值码截图')
     #         self.xpass_page.Copy_Address_Memo()
+    #         self.xpass_page.save_img('/BNB充值码截图')
     #     except BaseException:
-    #         print(f'无法获取BNB充值二维码，请检查截图...')
+    #         print(f'无法获取BNB充值二维码...')
     #
-    #
-    # def test_010_check_XPASS_BNB_transfer(self):
-    #     """检查XPASS卡的BNB的转账"""
-    #
-    #     self.xpass_page.into_XPASS_card()
-    #     self.xpass_page.into_coin_detail(coin='BNB')
-    #     self.xpass_page.Click_Transfer_with_memo(address='23423534543dfhhfghghjgf')
-    #     time.sleep(1)
-    #     self.xpass_page.send_codes()  # 输入转账时的验证码
-    #     time.sleep(1)
-    #     self.xpass_page.confirm_transfer()
-    #     self.assertTrue(self.xpass_page.is_toast_exist('transfer'), '判断是否转账成功')
+    def test_010_check_XPASS_BNB_transfer(self):
+        """检查XPASS卡的BNB的转账"""
+
+        time.sleep(2)
+        self.xpass_page.into_XPASS_card()
+        time.sleep(2)
+        self.xpass_page.into_coin_detail(coin='BNB')
+        time.sleep(3)
+        self.xpass_page.BEP2_transfer(address='23423534543dfhhfghghjgf')
+        time.sleep(1)
+        self.xpass_page.send_codes()  # 输入转账时的验证码
+        time.sleep(1)
+        self.xpass_page.confirm_transfer()
+        self.assertTrue(self.xpass_page.is_toast_exist('transfer'), '判断是否转账成功')
 
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=0)
